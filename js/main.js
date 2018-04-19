78fe19497ed15f37beacd840773cb5e40ec5113a
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2018-04-19 15:53:12 +0200

    Enchufando los enlaces

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-(function(){
-    init();
+$(document).ready(function () {
+  init();
 
-    function init(){
-        document.querySelector('.close-container').addEventListener('click', function(){
-            document.getElementById('input-menu').checked = false;
-        });
+  function init() {
+    $('.close-container').click(function () {
+      $('#input-menu').prop('checked', false);
+    });
 
-        document.querySelector('.hamburger-container').addEventListener('click', function(){
-            document.getElementById('input-menu').checked = true;
-        });
-    }
-})()
+    $('.hamburger-container').click(function () {
+      $('#input-menu').prop('checked', true);
+    });
+
+    $('.js-tab .tab-nav-item').click(function (event) {
+      event.preventDefault();
+      const $id = $(this).attr('href')
+      $('.js-tab .tab-nav-item--on').removeClass('tab-nav-item--on');
+      $('.js-tab .tab-content--on').removeClass('tab-content--on');
+
+      $(this).addClass('tab-nav-item--on');
+      $('.js-tab ' + $id).addClass('tab-content--on');
+    });
+  }
+});
