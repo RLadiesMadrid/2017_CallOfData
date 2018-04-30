74bc892f7e3e88156e8756ef43afb3b51c27789f
Ruben Gomez <rgomez.bertoli@gmail.com>
2018-04-30 17:55:45 +0200

    Cambio de idioma a español incluido

--- js/lang.js
+++ js/lang.js
@@ -1,26 +1,25 @@
-$(document).ready(function () {
-  const langs = {
-    es: 'es-ES',
-    en: 'en-GB'
-  };
-  const lang = langs[getParameterByName('lang')] || 'es-ES';
+const langs = {
+  es: 'es-ES',
+  en: 'en-GB'
+};
 
+const lang = langs[getParameterByName('lang')] || 'es-ES';
+
+$(document).ready(function () {
   $.getJSON('assets/lang/' + lang + '.json', function (json) {
     for (item in json) {
       $('[data-lang="' + item + '"]').html(json[item]);
       $('[data-lang-placeholder="' + item + '"').attr('placeholder', json[item]);
     }
   });
-
-  function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-      results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }
-
-
 });
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
