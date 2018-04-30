8d9e1988c23bc058b36140a9afc96426ce45b236
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2018-04-30 11:55:03 +0200

    Nuevo webserver de gulp para que ejecute las webs en local

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function () {
+	gulp.src('./').pipe(webserver({
+			livereload: true,
+			host: '127.0.0.1',
+			port: 8080,
+			open: true,
+			fallback: './index.html'
+		}));
+});
 
 gulp.task('sass', function () {
 	return gulp.src('./assets/scss/**/*.scss')
@@ -16,4 +27,4 @@ gulp.task('sass:watch', function () {
 });
 
 gulp.task('build', ['sass']);
-gulp.task('default', ['sass:watch']);
+gulp.task('default', ['sass:watch', 'webserver']);
