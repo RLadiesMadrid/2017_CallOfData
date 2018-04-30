16405fedd6ec3b420d61e75901182fe8a6fb2ffb
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2018-04-30 11:04:05 +0200

    Primeros cambios en el idioma

--- js/lang.js
+++ js/lang.js
@@ -1,6 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  const langs = {
+    es: 'es-ES',
+    en: 'en-GB'
+  };
+  const lang = langs[getParameterByName('lang')] || 'es-ES';
 
-  $.getJSON('assets/lang/esES.json', function (json) {
-    console.log(json);
-  })
+  $.getJSON('assets/lang/' + lang + '.json', function (json) {
+    for (item in json) {
+      $('[data-lang="' + item + '"]').html(json[item]);
+      $('[data-lang-placeholder="' + item + '"').attr('placeholder', json[item]);
+    }
+  });
+
+  function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
 });
